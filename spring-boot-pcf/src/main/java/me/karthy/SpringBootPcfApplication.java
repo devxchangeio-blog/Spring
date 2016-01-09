26cd57027e7f583c9d85f477975ee0b4a8cb369d
karthikeyan <karthy86@live.com>
2016-01-09 08:04:07 -0500

    initial commit

---
+++ spring-boot-pcf/src/main/java/me/karthy/SpringBootPcfApplication.java
@@ -0,0 +1,12 @@
+package me.karthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPcfApplication.class, args);
+	}
+}
